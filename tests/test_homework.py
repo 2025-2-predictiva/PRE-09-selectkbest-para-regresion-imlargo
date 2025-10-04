7e45dda7c5355d804a7bdaeef726bffa25cc7616
imlargo <jlargob@unal.edu.co>
2025-10-04 08:54:48 -0500

    _

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -31,14 +31,14 @@ def load_estimator():
 
 def test_01():
 
-    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import r2_score
 
     x, y = load_data()
     estimator = load_estimator()
 
-    accuracy = accuracy_score(
-        y_true=y,
-        y_pred=estimator.predict(x),
+    r2 = r2_score(
+        y, 
+        estimator.predict(x),
     )
 
-    assert True
+    assert r2 >= 0
